27afd85d128c2e881c7885a6877e634f54240fc6
Pherrows <yhk1492@naver.com>
2023-04-19 00:10:53 +0900

    4-19 집

--- STAR/js/main.js
+++ STAR/js/main.js
@@ -1,7 +1,7 @@
 const coverEl = document.querySelector("body .cover");
 
 window.addEventListener('scroll', function () {
-  if (window.scrollY > 1500) {
+  if (window.scrollY > 1800) {
     coverEl.style.display = 'flex';
   } else {
     coverEl.style.display = 'none';
